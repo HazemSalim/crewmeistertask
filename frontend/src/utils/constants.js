91bc6b35ba0d515b7cb4894a45249f9617c988d1
hazemsalim <haz.future@gmail.com>
2022-04-05 11:49:09 +0300

    Update API to local Host

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-//export const API_URL = "http://localhost:5000";
-export const API_URL = "https://absences-app-task.herokuapp.com";
+export const API_URL = "http://localhost:5000";
+//export const API_URL = "https://absences-app-task.herokuapp.com";
